3f03f12cef6141806a6a4e4a15e8cd63af5561b4
david <edinson.leon@ia-aplicada.com>
2024-10-06 00:59:23 -0500

    Bug fixed

--- backend/app/config/__init__.py
+++ backend/app/config/__init__.py
@@ -12,8 +12,8 @@ class Settings(BaseSettings):
     MAIL_FROM_NAME: str = "Notificación registro a nuevo Fondo"
 
     # Configuración de Twilio
-    TWILIO_ACCOUNT_SID: str
-    TWILIO_AUTH_TOKEN: str
+    TWILIO_ACCOUNT_SID: str = "ACe8ae20d40445f88e82efcc23c2481663"
+    TWILIO_AUTH_TOKEN: str = "607d521b63ae473ca3a2c37bc2362bef"
     TWILIO_PHONE_NUMBER: str = "+19255280265"
 
 settings = Settings()
